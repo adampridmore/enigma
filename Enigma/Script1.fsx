cfbb7907c509277701362fe4425421da2725ea69
Adam Pridmore <adam_pridmore@hotmail.com>
2015-12-11 11:20:55 +0000

    - Initial commit of V1 and V2 enigma.

---
+++ Enigma/Script1.fsx
@@ -0,0 +1,24 @@
+﻿let numberOfLetters = 4 
+
+let rotor1 position l =
+    (position + l) % numberOfLetters
+    |>  function 
+        | 0 -> 1
+        | 1 -> 2
+        | 2 -> 3
+        | 3 -> 0
+        | _ -> failwith (sprintf "Letter outside range: %d" l)
+
+let reflector l = (l + (numberOfLetters/2) ) % numberOfLetters
+let plaintext = [2;1;3;1]
+let plaintext2 = seq{for i = 1 to 100 do yield 1} |> Seq.toList
+
+let encrypt = 
+    Seq.mapi (fun i letter -> letter |> rotor1 i |> reflector |> rotor1Reverse i) 
+    >> Seq.toList
+
+//let cipherText = plaintext |> encrypt |> encrypt
+let cipherText = plaintext2 |> encrypt 
+
+
+
---
+++ Enigma/Script2.fsx
@@ -0,0 +1,52 @@
+﻿let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let rotor1Mapping = "BCDEFGHIJKLMNOPQRSTUVWXYZA"
+let numberOfLetters = alphabet.Length
+
+let toUpper (s:string) = s.ToUpper()
+let toCharArray (s:string) = s.ToCharArray()
+let charToString (c:char) = c.ToString()
+let split (s:string) = s.Split([|System.Environment.NewLine;"\n";"\r\n"|], System.StringSplitOptions.RemoveEmptyEntries)
+    
+let indexOf (toFind:char) (text:string) = 
+    match text.IndexOf(toFind) with
+    | -1 -> failwith (sprintf "Unknown char '%c'" toFind)
+    | i -> i
+
+let charFromString (text:string) index = 
+    let s = text.Substring(index, 1)
+    s.Chars(0)
+
+let subString (text:string) index =
+    text.Substring(index, 1)
+    
+
+
+let rotorB1 (l:char) =
+    alphabet 
+    |> indexOf l 
+    |> charFromString rotor1Mapping
+
+let rotorB1Reverse (l:char) =
+    rotor1Mapping
+    |> indexOf l 
+    |> charFromString alphabet
+
+let reflector (l:char) = 
+    ((alphabet |> indexOf l) + (numberOfLetters / 2)) % numberOfLetters 
+    |> charFromString alphabet
+
+let cipherChar = rotorB1 >> reflector >> rotorB1Reverse
+// let cipherString text = text
+    
+alphabet.ToCharArray() 
+|> Seq.map (fun c -> c |> rotorB1 |> reflector |> rotorB1Reverse)
+|> Seq.iter (printfn "%c")
+
+'B' |> rotorB1 |> reflector |> rotorB1Reverse
+
+"Hello"
+|> toUpper
+|> toCharArray
+|> Seq.map (fun c -> c |> rotorB1 |> reflector |> rotorB1Reverse)
+|> Seq.map charToString
+|> Seq.reduce (+)
